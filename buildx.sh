5e70dc661a6681b7933e0f3430109a36a8493cb1
mc303 <daniel@buonocore.nl>
2023-08-16 16:17:45 +0200

    update buildx

--- buildx.sh
+++ buildx.sh
@@ -8,6 +8,13 @@ CONTAINER_NAME_TAG_VERSION="ghcr.io/mc303/caddy-transip:${CADDY_VERSION:1}"
 BUIILDX_REPO='build-caddy-transip'
 BUILD_PLATFORM="linux/amd64,linux/arm/v7,linux/arm64"
 
+echo ${DOCKER_USER_REPO}
+echo ${DOCKER_API_URL}
+echo ${CADDY_VERSION:1}
+echo ${DOCKER_REPO}
+echo ${CONTAINER_NAME_TAG_VERSION}
+echo ${BUIILDX_REPO}
+echo ${BUILD_PLATFORM}
 
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
@@ -17,7 +24,7 @@ docker buildx create --platform=${BUILD_PLATFORM} --name ${BUIILDX_REPO}
 docker buildx use ${BUIILDX_REPO}
 
 #build multiplatform docker image
-docker buildx build --platform=${BUILD_PLATFORM} --tag ${CONTAINER_NAME} --tag ${CONTAINER_NAME_TAG_VERSION} --push . --build-arg CADDY_VERSION=${CADDY_VERSION:1}
+docker buildx build . --platform=${BUILD_PLATFORM} --tag ${CONTAINER_NAME} --tag ${CONTAINER_NAME_TAG_VERSION} --build-arg CADDY_VERSION=${CADDY_VERSION:1} --push
 
 #remove multiplatform environment
 docker buildx rm ${BUIILDX_REPO}
