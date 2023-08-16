e407289cbc7e97fab3f6f24ad7770593a19bdaff
mc303 <daniel@buonocore.nl>
2023-08-16 14:04:47 +0200

    updated build.sh & buildx.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,17 @@
 #!/usr/bin/env bash
 #env DOCKER_BUILDKIT=1 docker build --no-cache -t mc303/hass-node-red .
 #env DOCKER_BUILDKIT=1 
-#create platform buildx env
 
-CONTAINER_NAME="ghcr.io/mc303/caddy-transip"
+#create platform buildx env
+export CADDY_VERSION=$(curl --silent ${DOCKER_API_URL} | grep -Po '"tag_name": "\K.*\d')
+
+CONTAINER_NAME="ghcr.io/mc303/caddy-transip:latest"
+CONTAINER_NAME_TAG_VERSION="ghcr.io/mc303/caddy-transip:${CADDY_VERSION:1}"
 DOCKER_USER_REPO=caddyserver/caddy
 DOCKER_API_URL=https://api.github.com/repos/${DOCKER_USER_REPO}/releases/latest 
 
-export CADDY_VERSION=$(curl --silent ${DOCKER_API_URL} | grep -Po '"tag_name": "\K.*\d')
- 
 
+docker build --no-cache --tag ${CONTAINER_NAME} --tag ${CONTAINER_NAME_TAG_VERSION} --push . --build-arg CADDY_VERSION=${CADDY_VERSION:1}
 
-# BUILD_PLATFORM="linux/amd64,linux/arm/v7,linux/arm64"
-# BUILD_NAME="build-caddy-transip"
-
-# docker buildx create --platform $BUILD_PLATFORM --name $BUILD_NAME
-# docker buildx use $BUILD_NAME
-
-# # build platforms
-# docker buildx build --platform=$BUILD_PLATFORM  -t $CONTAINER_NAME --push .
-
-# # remove build env
-# docker buildx rm $BUILD_NAME
-
-docker build --no-cache -t $CONTAINER_NAME . --build-arg CADDY_VERSION=${CADDY_VERSION:1}
+##  --rm 	true 	Remove intermediate containers after a successful build
+## --tag , -t 		Name and optionally a tag in the name:tag format
--- buildx.sh
+++ buildx.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+export CADDY_VERSION=$(curl --silent ${DOCKER_API_URL} | grep -Po '"tag_name": "\K.*\d')
+
 DOCKER_REPO="ghcr.io/mc303/caddy-transip:latest"
+CONTAINER_NAME_TAG_VERSION="ghcr.io/mc303/caddy-transip:${CADDY_VERSION:1}"
 BUIILDX_REPO='build-caddy-transip'
 BUILD_PLATFORM="linux/amd64,linux/arm/v7,linux/arm64"
+
 DOCKER_USER_REPO=caddyserver/caddy
 DOCKER_API_URL=https://api.github.com/repos/${DOCKER_USER_REPO}/releases/latest 
-
-export CADDY_VERSION=$(curl --silent ${DOCKER_API_URL} | grep -Po '"tag_name": "\K.*\d')
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
@@ -15,7 +17,7 @@ docker buildx create --platform=${BUILD_PLATFORM} --name ${BUIILDX_REPO}
 docker buildx use ${BUIILDX_REPO}
 
 #build multiplatform docker image
-docker buildx build --platform=${BUILD_PLATFORM} -t ${DOCKER_REPO} --push . --build-arg CADDY_VERSION=${CADDY_VERSION:1}
+docker buildx build --platform=${BUILD_PLATFORM} --tag ${CONTAINER_NAME} --tag ${CONTAINER_NAME_TAG_VERSION} --push . --build-arg CADDY_VERSION=${CADDY_VERSION:1}
 
 #remove multiplatform environment
 docker buildx rm ${BUIILDX_REPO}
